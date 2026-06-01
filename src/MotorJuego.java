5460493f673c548558d92fad17732b97966a4049
PABLO MANUEL SOLANO SALINAS <pmsolano@alu.fpsanantonio.com>
2026-06-01 11:02:36 +0200

    feat: implementar estados y gestión de entidades

---
+++ src/MotorJuego.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MotorJuego {
+
+    private String estado;
+    private List<EntidadVideojuego> entidades;
+
+    public MotorJuego() {
+        estado = "MENU";
+        entidades = new ArrayList<>();
+    }
+
+    public void iniciarPartida() {
+        estado = "JUGANDO";
+    }
+
+    public void pausar() {
+        estado = "PAUSA";
+    }
+
+    public void reanudar() {
+        estado = "JUGANDO";
+    }
+
+    public void gameOver() {
+        estado = "GAME_OVER";
+    }
+
+    public void agregarEntidad(EntidadVideojuego entidad) {
+        entidades.add(entidad);
+    }
+
+    public void eliminarEntidad(EntidadVideojuego entidad) {
+        entidades.remove(entidad);
+    }
+
+    public List<EntidadVideojuego> getEntidades() {
+        return entidades;
+    }
+
+    public void actualizar() {
+
+        System.out.println("\n=== GAME LOOP ===");
+
+        for (EntidadVideojuego entidad : entidades) {
+
+            entidad.mover(1, 0);
+
+            System.out.println(
+                    entidad.getNombre()
+                            + " -> (" +
+                            entidad.getX() +
+                            "," +
+                            entidad.getY() +
+                            ")");
+        }
+    }
+}
